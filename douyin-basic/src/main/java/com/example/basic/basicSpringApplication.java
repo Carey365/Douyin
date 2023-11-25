883920e98386ec25e8a90bb67205553f0f578183
Carey <clh@bupt.edu.cn>
2023-11-25 22:23:54 +0800

    basic模块初始化

---
+++ douyin-basic/src/main/java/com/example/basic/basicSpringApplication.java
@@ -0,0 +1,15 @@
+package com.example.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author chenlianghao
+ */
+@SpringBootApplication
+public class basicSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(basicSpringApplication.class, args);
+    }
+}
